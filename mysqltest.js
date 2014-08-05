082ade2360d3081c3494cc243239c8a437bf5477
Junyong Suh <junyongsuh@gmail.com>
2014-08-05 02:34:12 +0000

    Commit #2

---
+++ mysqltest.js
@@ -0,0 +1,35 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+	host     : 'wordpress-free.cm6jrr76me8k.us-east-1.rds.amazonaws.com',
+	user     : 'zechery',
+	password : 'shekap1226',
+});
+
+connection.connect();
+
+var query = connection.query('SELECT * FROM wordpress.wp_users');
+query
+  .on('error', function(err) {
+    // Handle error, an 'end' event will be emitted after this as well
+  })
+  .on('fields', function(fields) {
+    // the field packets for the rows to follow
+  })
+  .on('result', function(row) {
+    // Pausing the connnection is useful if your processing involves I/O
+    connection.pause();
+
+    processRow(row, function() {
+      connection.resume();
+    });
+  })
+  .on('end', function() {
+    // all rows have been received
+  });
+
+connection.end();
+
+// process mysql result row
+function processRow(row) {
+	console.log(row);
+}
